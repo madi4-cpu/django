039ee7c127e5defbe8e083372d7f3be44dab3367
madikara <madikaragoy@gmail.com>
2026-09-09 10:45:30 +0500

    Fix models, tests and workflow

--- django-ci/core/tests.py
+++ django-ci/core/tests.py
@@ -1,15 +1,25 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
-from core.models import Product
+from products.models import Note
 
 
-class Note:
-    def __init__(self, title, text):
-        self.title = title
-        self.text = text
-    def created_at(self):
-        return "12:00"
+class NoteModelTest(TestCase):
 
-    def Test_1(self):
-        self.assertEqual(self.title, "тест заметка")
-        self.assertEqual(self.text, "тест заметка")
-        self.assertEqual(self.created_at(), "12:00")
+    def test_note_creation(self):
+        note = Note.objects.create(
+            title="Тестовая заметка",
+            text="Текст тестовой заметки"
+        )
+        self.assertEqual(note.title, "Тестовая заметка")
+        self.assertEqual(note.text, "Текст тестовой заметки")
+        self.assertIsNotNone(note.created_at)
+
+    def test_title_is_required(self):
+        note = Note(title="", text="Текст без названия")
+        with self.assertRaises(ValidationError):
+            note.full_clean()
+
+    def test_note_exists_in_db(self):
+        initial_count = Note.objects.count()
+        Note.objects.create(title="Заметка 2", text="Текст 2")
+        self.assertEqual(Note.objects.count(), initial_count + 1)
